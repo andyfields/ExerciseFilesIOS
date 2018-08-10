ab707fa984652c3350894a5b0f1f0ce8dd165c8c
andyfields <afields1957-github@yahoo.com>
2018-08-09 22:41:28 -0700

    Add CameraTest changes.

---
+++ CameraTest/CameraTest/CameraTest/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  CameraTest
+//
+//  Created by Andrew Fields on 8/9/18.
+//  Copyright © 2018 Andrew Fields. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func AddPhoto(_ sender: Any) {
+  }
+  
+}
+
+
+
