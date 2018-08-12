3ec2c83db6e11579fda5799be3396a9a299fafc1
andyfields <afields1957-github@yahoo.com>
2018-08-12 11:52:48 -0700

    First working image picker.

--- CameraTest/CameraTest/CameraTest/FirstViewController.swift
+++ CameraTest/CameraTest/CameraTest/FirstViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+  
+  static let shared = CameraHandler()
+  
+  @IBOutlet weak var displayImage: UIImageView!
+  
+  fileprivate var currentVC: UIViewController!
+  
+  //MARK: Internal Properties
+  var imagePickedBlock: ((UIImage) -> Void)?
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -21,6 +30,10 @@ class FirstViewController: UIViewController {
   }
 
   @IBAction func AddPhoto(_ sender: Any) {
+    CameraHandler.shared.showActionSheet(vc: self)
+    CameraHandler.shared.imagePickedBlock = { (image) in
+      self.displayImage.image = image
+    }
   }
   
 }
